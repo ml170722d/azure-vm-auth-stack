6d9e2d967d63a3796a37a095411984b7c559d4a3
Luka Matovic <luka.matovic@p3-group.com>
2025-05-28 02:41:05 +0200

    Add tf virtual network and subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,19 @@ resource "azurerm_resource_group" "rg" {
   name     = "hylastix"
   location = var.location
 }
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${azurerm_resource_group.rg.name}-vnet"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Create a virtual network subnet
+resource "azurerm_subnet" "subnet" {
+  name = "${azurerm_resource_group.rg.name}-subnet"
+  address_prefixes = [ "10.0.1.0/24" ]
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.hylastix-vnet.name
+}
